d3b9135b7d3f0b386014e3454f4ab86b1b58797c
ShiftIns <wujunkui407@icloud.com>
2024-07-17 22:34:26 +0800

    develop

--- UI/UI.cs
+++ UI/UI.cs
@@ -4,11 +4,14 @@ namespace UI
 {
     public class UI : MonoBehaviour
     {
+        [SerializeField] private Transform header;
+        [SerializeField] private Transform contentParent;
+        
         public void SwitchTo(GameObject _menu)
         {
             for (int i = 0; i < transform.childCount; i++)
             {
-                transform.GetChild(i).gameObject.SetActive(false);
+                contentParent.GetChild(i).gameObject.SetActive(false);
             }
 
             if (_menu != null)
